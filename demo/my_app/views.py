78da24ad41218ca6178e801f14a4bf3736ba141b
prathikshaa.selva <prathikshaa.selva@aspiresys.com>
2024-04-05 22:25:12 +0530

    05-04-2024

---
+++ demo/my_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html',{'name':'prathi'})
+
+def add(request):
+    first_value=int(request.POST['number1'])
+    second_value=int(request.POST['number2'])
+    final_result = first_value+second_value
+
+    return render(request,"result.html",{'result':final_result})
